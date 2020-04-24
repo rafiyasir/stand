e1c37c1ef6b3013af60aa8e84bbca7fbb97f3b77
rafiyasir <40689559+rafiyasir@users.noreply.github.com>
2020-04-24 18:09:26 +0530

    change content

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+const navbar = document.querySelector('.transparent');
+const body = document.querySelector('body');
+
+
 var currentEle = $("#radial-bg");
 var nextEle, count = 1;
 var myInterval = setInterval(function() {
@@ -17,14 +21,14 @@ text.textContent = "";
 
 for(let i = 0; i < splitText.length; i++){
     let char = (splitText[i] === " ") ? "&nbsp;" : splitText[i];
-    text.innerHTML += `<span>${char}</span>`;
+    text.innerHTML += `<span class="main-tag">${char}</span>`;
 }
 
 let char = 0;
 let timer = setInterval(onTick, 50);
 
 function onTick(){
-    const span = text.querySelectorAll('span')[char];
+    const span = text.querySelectorAll('.main-tag')[char];
     span.classList.add('fade');
     char++;
     if(char === splitText.length){
@@ -37,3 +41,69 @@ function complete(){
     clearInterval(timer);
     timer = null;
 }
+
+// if(scrollY <= 20){
+//   body.style.overflow = 'hidden';
+//   setTimeout(() => body.style.overflow = 'auto', 8000);
+// }
+// else{
+//   body.style.overflow = 'auto';
+// }
+
+ let flag = 1;
+ 
+ const tag = document.querySelector('.tag-line');
+ const strTag = tag.textContent;
+ const splitTag = strTag.split("");
+ tag.textContent = "";
+
+window.addEventListener('scroll', function() {
+  let scroll = window.pageYOffset;
+  // document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px';
+
+  if(scrollY > 300 && flag === 1) {
+    flag++;
+  
+    for(let i = 0; i < splitTag.length; i++){
+      let char = (splitTag[i] === " ") ? "&nbsp;" : splitTag[i];
+      tag.innerHTML += `<span class="tag-line-span">${char}</span>`;
+    }
+      
+    let charTag = 0;
+    let timerTag = setInterval(onTickTag, 50);
+  
+    function onTickTag(){
+      const span = tag.querySelectorAll('.tag-line-span')[charTag];
+      span.classList.add('fade-tag');
+      charTag++;
+      if(charTag === splitTag.length){
+          completeTag();
+          return;
+      }
+    }
+  
+    function completeTag(){
+      clearInterval(timerTag);
+      timerTag = null;
+    }
+  }
+
+  //For navbar color
+  if(scroll <= 600){
+    if(scroll/500 <= 1){
+      navbar.style.backgroundImage = `linear-gradient(to bottom right, rgba(141,188,250,${scroll/500}), rgba(169, 175, 183,${(scroll)/500}), rgba(169,181,195,${(scroll)/500}))`;
+    }
+
+    body.style.backgroundImage = `linear-gradient(to bottom right, rgba(141,188,250,1), rgba(169, 175, 183,1), rgba(169,181,195,1))`;
+  }
+  else {
+    navbar.style.backgroundImage = `linear-gradient(to bottom right, rgba(141,188,250,1), rgba(169, 175, 183,1), rgba(169,181,195,1))`;
+
+    if(scroll <=2000){
+    body.style.backgroundImage = `linear-gradient(to bottom right, rgba(141,188,250,${1-(scroll)/4000}), rgba(169, 175, 183,${1-(scroll)/4000}), rgba(169,181,195,${1-(scroll)/4000}))`;
+    }
+    if(scroll > 2000) {
+      body.style.backgroundImage = `linear-gradient(to bottom right, rgba(141,188,250,0.7), rgba(169, 175, 183,0.5), rgba(169,181,195,0.5))`;
+    }
+  }
+});
